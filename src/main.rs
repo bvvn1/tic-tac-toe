aa1d38cb95d5ec85495f80e4982c79170b1843b0
bvvn1 <bvvn@protonmail.com>
2025-08-31 04:01:22 +0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let mut grid:[[char; 3]; 3] = 
+    [[' ', ' ', ' '],
+     [' ', ' ', ' '],
+     [' ', ' ', ' ']
+    ];
+
+
+    for row in &grid{
+        println!("{:?}", row)
+
+        
+    }
+    
+}
+
+
