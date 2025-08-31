f23f699681cff13edd483403eed717fc8cad3baf
bvvn1 <bvvn@protonmail.com>
2025-08-31 19:12:50 +0300

    fixed input and cursor  deleting

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use crossterm::event::{read, Event, KeyEventKind, KeyCode, poll};
 use crossterm::terminal::ClearType;
 use crossterm::{event, execute, terminal};
 use std::{thread::sleep, time::Duration};
 use std::io::{stdout, Write};
+
+
+
+
+
 
 fn main() {
     let mut cursor: (usize, usize) =(0, 0);
@@ -9,7 +15,6 @@ fn main() {
         panic!("Out of bounds");
     }
     
-
     draw_game(&mut cursor);
     
 
@@ -17,7 +22,6 @@ fn main() {
    
     
 }
-
 
 fn draw_game(cursor: &mut (usize, usize)){
 
@@ -28,7 +32,7 @@ fn draw_game(cursor: &mut (usize, usize)){
      [' ', ' ', ' '],
      [' ', ' ', ' ']
     ];
-
+    let mut prev = *cursor;
 
     
 
@@ -36,11 +40,16 @@ fn draw_game(cursor: &mut (usize, usize)){
     loop{
 
 
+        execute!(stdout(), terminal::Clear(ClearType::All));
+
+
+        grid[prev.0][prev.1] = ' ';
+
         handle_inputs(cursor);
 
         grid[cursor.0][cursor.1] = '_';
-
-        print!("{:?}", cursor);
+        
+        prev = *cursor;
 
          
         for row in &grid{
@@ -49,16 +58,47 @@ fn draw_game(cursor: &mut (usize, usize)){
         
         sleep(Duration::from_millis(1000));
 
-        execute!(stdout(), terminal::Clear(ClearType::All));
+        
     
 
     }
 
 }
 
-
 fn handle_inputs(cursor: &mut (usize, usize)) {
     
+    terminal::enable_raw_mode();
 
-
+    if poll(Duration::from_millis(100)).unwrap() {
+        if let Event::Key(event) = read().unwrap() {
+            if event.kind == KeyEventKind::Press {
+                match event.code {
+                    KeyCode::Up => {
+                        if cursor.0 > 0 {
+                            cursor.0 -= 1;
+                        }
+                    }
+                    KeyCode::Down => {
+                        if cursor.0 < 2 {
+                            cursor.0 += 1;
+                        }
+                    }
+                    KeyCode::Right => {
+                        if cursor.1 < 2 {
+                            cursor.1 += 1;
+                        }
+                    }
+                    KeyCode::Left => {
+                        if cursor.1 > 0 {
+                            cursor.1 -= 1;
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+    terminal::disable_raw_mode();
 }
+
+
