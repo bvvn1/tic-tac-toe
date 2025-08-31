a33b2935df9247c6adb18f60b1396cbf2d18b4f0
bvvn1 <bvvn@protonmail.com>
2025-08-31 04:01:38 +0300

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,47 @@
+use inputbot;
+use std::{thread::sleep, time::Duration};
+use std::io::Write;
+
 fn main() {
+
+
+    draw_game();
+    
+    
+}
+
+
+fn draw_game(){
+
+    let mut cursor: (usize, usize) =(0, 0);
+
     let mut grid:[[char; 3]; 3] = 
     [[' ', ' ', ' '],
      [' ', ' ', ' '],
      [' ', ' ', ' ']
     ];
 
+    //infinite loop
+    loop{
 
-    for row in &grid{
+        grid[cursor.0][cursor.1] = '_';
+
+        for row in grid{
         println!("{:?}", row)
+        }
 
-        
+        sleep(Duration::from_millis(1000));
+
+
+        clear_console();
+
     }
-    
+
 }
 
-
+fn clear_console() {
+    // The ANSI escape code for clearing the terminal screen
+    print!("\x1B[2J\x1B[1;1H");
+    // Flush the output to ensure the console is cleared immediately
+    std::io::stdout().flush().unwrap();
+}
