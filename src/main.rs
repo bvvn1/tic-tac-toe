7919252f8be4f4e6c8a0a3c57e494c4624646dd8
bvvn1 <bvvn@protonmail.com>
2025-08-31 21:57:57 +0300

    neshto

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+use crossterm::event::{read, Event, KeyEventKind, KeyCode, poll};
+use crossterm::terminal::ClearType;
+use crossterm::{event, execute, terminal};
+use std::{thread::sleep, time::Duration};
+use std::io::{stdout, Write};
+
 
 #[derive(Debug)]
 struct Cursor{
-    x: i8,
-    y: i8
+    x: usize,
+    y: usize
 }
 
 impl Cursor{
@@ -12,6 +18,11 @@ impl Cursor{
             y: 0
         }
     }
+    pub fn goto(&mut self, x: usize, y: usize) {
+        self.x = x;
+        self.y = y;
+    }
+
 }
 
 #[derive(Debug)]
@@ -37,6 +48,76 @@ impl Game{
 
 fn main(){
     let mut game = Game::new();    
-    
+    game.cursor.goto(1, 2);
+    draw_game(&mut game);
+
 }
 
+
+fn draw_game(g: &mut Game){
+
+
+
+
+
+    loop{
+
+        execute!(stdout(), terminal::Clear(ClearType::All));
+
+        g.grid[g.prev.x][g.prev.y] = ' ';
+
+        handle_inputs(&mut g.cursor);
+
+        g.grid[g.cursor.x][g.cursor.y] = '_';
+
+        g.prev.x = g.cursor.x;
+        g.prev.y = g.cursor.y;
+
+
+
+        for rows in g.grid{
+            println!("{:?}", rows);
+        }
+        
+        sleep(Duration::from_millis(1000));
+
+    }
+
+}
+
+
+fn handle_inputs(cursor: &mut Cursor) {
+
+    terminal::enable_raw_mode();
+
+    if poll(Duration::from_millis(100)).unwrap() {
+        if let Event::Key(event) = read().unwrap() {
+            if event.kind == KeyEventKind::Press {
+                match event.code {
+                    KeyCode::Up => {
+                        if cursor.y > 0 {             //tuka ima out of bounds fixni go tupak
+                            cursor.y += 1;
+                        }
+                    }
+                    KeyCode::Down => {
+                        if cursor.y < 2 {
+                            cursor.y -= 1;
+                        }
+                    }
+                    KeyCode::Right => {
+                        if cursor.x < 2 {
+                            cursor.x += 1;
+                        }
+                    }
+                    KeyCode::Left => {
+                        if cursor.x > 0 {
+                            cursor.x -= 1;
+                        }
+                    }
+                    _ => {}
+                }
+            }
+        }
+    }
+    terminal::disable_raw_mode();
+}
